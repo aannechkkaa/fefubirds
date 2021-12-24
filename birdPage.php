1b2df0b2157c435c07821d7badc976f782cf124d
aannechkkaa <96425807+aannechkkaa@users.noreply.github.com>
2021-12-24 15:02:33 +1000

    Update birdPage.php

--- birdPage.php
+++ birdPage.php
@@ -4,91 +4,83 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="Styles/BirdPageStyles.css">
         <link rel="stylesheet" href="FooterHeader/FHstyles.css">
-        <script src="header.js"></script>
         <title>FefuBirds</title>
     </head>
     <?php include("FooterHeader/header.php")?>
     <body>
+        <?php
+        session_start();
+        $bid = $_GET['id'];
+        include("connect.php");
+        $sql = "SELECT * FROM `birds` WHERE id='$bid'";
+        $result = mysqli_query($conn, $sql);
+        if($row = $result->fetch_assoc()){
+        ?>
             <div class = "TitleBackground">
-                Название вида
+                <?php echo($row['birdName']) ?>
             </div>
             <div class = "listen">
                 <p class = "listenText">
                     Слушать щебет
                 </p>
-                <img src = "img/PLAY.png" style = "height: 70%" class="listenelement">
+                <img src = "img/PLAY.png" style = "height: 70%">
                 </img>
-                <img src = "img/Audio.png" style = "height: 80%" class="listenelement">
+                <img src = "img/Audio.png" style = "height: 80%">
                 </img>
             </div>
         <div class = "informationBackground">
-            <img src = "img/ExampleUnique.png" style = "height: 80%">
+            <img src = "birdPic/<?php echo($row['birdPicture']) ?>" style = "height: 80%">
             </img>
             <p class = "information">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Praesent volutpat, metus id iaculis aliquam, neque ex finibus nisl,
-                id blandit tellus erat at nunc. Nam vitae sapien porta, facilisis nisi ac,
-                tristique tortor. Etiam at massa vehicula, rhoncus elit vitae, porta felis.
-                Pellentesque viverra, justo eget imperdiet aliquet, tellus diam tempus neque,
-                eu bibendum libero lacus a leo. Phasellus rhoncus blandit ligula, et elementum
-                erat iaculis ac. Sed pretium felis at mi convallis tempus. Praesent dui urna,
-                ultrices a erat ut, faucibus mattis diam.
+                <?php echo($row['description']) ?>
             </p>
         </div>
+
+        <?php } else {
+            header("Location: /index.php");
+        } ?>
+
+
         <div class = "commentsTitle">
             КОММЕНТАРИИ
         </div>
         <div class = "commentsBox">
             <div class = "commentsBackground">
+                <?php
+                $sql = "SELECT * FROM `comments` WHERE birdId='$bid'";
+                $cresult = mysqli_query($conn, $sql);
+                while($row = $cresult->fetch_assoc()){
+                    $cuid = $row['userId'];
+                    $sql = "SELECT * FROM `users` WHERE id='$cuid'";
+                    $uresult = mysqli_query($conn, $sql);
+                    $urow = $uresult->fetch_assoc();
+                ?>
                 <div class = "comment">
                     <div class = "left">
-                        <img src = "img/defaultpp.png" class = "profilePicture">
+                        <img src = "userAvatars/<?php echo($urow['avatar']);?>" class = "profilePicture">
                         </img>
-                    </div>
-                    <div class = "right">
-                        <p class = "name">Имя Фамилия</p>
-                        <p class = "commentText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Praesent volutpat, metus id iaculis aliquam, neque ex finibus nisl,
-                            id blandit tellus erat at nunc. Nam vitae sapien porta, facilisis nisi ac,
-                            tristique tortor. Etiam at massa vehicula, rhoncus elit vitae, porta felis.
-                            Pellentesque viverra, justo eget imperdiet aliquet, tellus diam tempus neque,
-                            eu bibendum libero lacus a leo. Phasellus rhoncus blandit ligula, et elementum
-                            erat iaculis ac. Sed pretium felis at mi convallis tempus. Praesent dui urna,
-                            ultrices a erat ut, faucibus mattis diam.
-                        </p>
-                        <img src = "img/ExampleUnique.png" class = "commentImg">
-                        </img>
-                    </div>
-                </div>
-                <div class = "commentSeptum"></div>
-                <div class = "comment">
-                    <div class = "left">
-                        <img src = "img/defaultpp.png" class = "profilePicture">
-                        </img>
-                    </div>
-                    <div class = "right">
-                        <p class = "name">Имя Фамилия</p>
-                        <p class = "commentText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Praesent volutpat, metus id iaculis aliquam, neque ex finibus nisl,
-                            id blandit tellus erat at nunc. Nam vitae sapien porta, facilisis nisi ac,
-                            tristique tortor. Etiam at massa vehicula, rhoncus elit vitae, porta felis.
-                            Pellentesque viverra, justo eget imperdiet aliquet, tellus diam tempus neque,
-                            eu bibendum libero lacus a leo. Phasellus rhoncus blandit ligula, et elementum
-                            erat iaculis ac. Sed pretium felis at mi convallis tempus. Praesent dui urna,
-                            ultrices a erat ut, faucibus mattis diam.
-                        </p>
-                        <img src = "img/ExampleUnique.png" class = "commentImg">
-                        </img>
-                    </div>
-                </div>
-                <div class = "commentSeptum"></div>
-            </div>
-            <from id = "commentForm">
-                <input name = "photo" type = "file"><br>
-                <textarea form = "commentForm" class = "inputText" name = "text" required type = "text" placeholder = "Оставить комментарий..."></textarea>
+                        <?php
+                        if ($_SESSION['ustatus'] == 1 || $_SESSION['uid'] == $cuid){ ?>
+                            <button style = "width: 80%; margin-left: 10%;" class = "Button" onclick = "location.href = 'deleteComment.php?id=<?php echo($row['id'].'&bid='.$bid); ?>'"> Удалить комментарий </button>
 
+                        <?php } ?>
+                    </div>
+                    <div class = "right">
+                        <p class = "name"><?php echo($urow['name'].' '.$urow['surname']);?></p>
+                        <p class = "commentText">
+                            <?php echo($row['commentText']); ?>
+                        </p>
+                        <img src = "userComments/<?php echo($row['commentImage']); ?>" class = "commentImg">
+                        </img>
+                    </div>
+                </div>
+                <div class = "commentSeptum"></div>
+                <?php } ?>
+            </div>
+            <form method = "post" id = "commentForm" enctype="multipart/form-data" action = "commentp.php?id=<?php echo($bid); ?>">
+                <input name = "photo" type = "file"><br>
+                <textarea form = "commentForm" class = "inputText" name = "text" required type = "text" placeholder = "Оставить комментарий..."> </textarea>
+                <input type = "submit" value = "Отправить" class = "Button">
             </form>
         </div>
     </body>
